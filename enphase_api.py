885403d4fca6b3687ac37ed26e8f19190180fda7
Christopher Adams <christopher.m.adams2@gmail.com>
2024-12-09 04:43:46 -0800

    requests for Enphase Battery details

---
+++ enphase_api.py
@@ -0,0 +1,28 @@
+import requests
+import yaml
+
+envoy_ip = "192.168.4.57"  # Replace with your Envoy's IP address
+url = f"http://{envoy_ip}/ivp/ensemble/inventory"
+token = "token"
+
+headers = {
+    "Authorization": f"Bearer {token}"
+}
+
+try:
+    response = requests.get(url, headers=headers, verify=False)
+    if response.status_code == 200:
+        data = response.json()
+        #max_capacity = data["battery"][0].get("maximumCapacity", None)
+        #if max_capacity:
+        #    print(f"Battery Maximum Capacity: {max_capacity / 1000} kWh")
+        #else:
+        #    print("Maximum capacity field not found in response.")
+        yaml_data = yaml.dump(data, sort_keys=False, default_flow_style=False)
+        with open("inventory.yaml", "w") as yaml_file:
+            yaml_file.write(yaml_data)
+        print(yaml_data)
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+except Exception as e:
+    print(f"Error: {e}")
